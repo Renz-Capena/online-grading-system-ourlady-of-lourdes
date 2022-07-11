5df348b9a2ec20ddd52359a34f0bb94d1535f8af
Renzyyyy <renzcollincapena187@gmail.com>
2022-07-11 09:52:03 +0800

    Database update

--- db/old db/ollc-online-grading.sql
+++ db/old db/ollc-online-grading.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 11, 2022 at 03:49 AM
+-- Generation Time: Jul 09, 2022 at 05:27 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.12
 
@@ -61,8 +61,7 @@ CREATE TABLE `grades` (
 --
 
 INSERT INTO `grades` (`id`, `user_id`, `f_1`, `f_2`, `f_3`, `f_4`, `e_1`, `e_2`, `e_3`, `e_4`, `s_1`, `s_2`, `s_3`, `s_4`, `m_1`, `m_2`, `m_3`, `m_4`, `h_1`, `h_2`, `h_3`, `h_4`, `p_1`, `p_2`, `p_3`, `p_4`) VALUES
-(23, '89', '70', 78, 0, 0, 78, 76, 0, 0, 74, 75, 0, 0, 70, 77, 0, 0, 80, 79, 0, 0, 75, 80, 0, 0),
-(24, '85', '78', 70, 0, 0, 70, 71, 0, 0, 80, 71, 0, 0, 89, 71, 0, 0, 91, 71, 0, 0, 70, 71, 0, 0);
+(20, '78', '78', 0, 0, 0, 80, 0, 0, 0, 91, 0, 0, 0, 76, 0, 0, 0, 85, 0, 0, 0, 84, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -74,16 +73,16 @@ CREATE TABLE `reminders` (
   `id` int(11) NOT NULL,
   `student_email` varchar(150) NOT NULL,
   `from` varchar(150) NOT NULL,
-  `message` varchar(500) NOT NULL,
-  `date` varchar(150) NOT NULL
+  `message` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `reminders`
 --
 
-INSERT INTO `reminders` (`id`, `student_email`, `from`, `message`, `date`) VALUES
-(27, 'renz@yahoo.com', 'teacher@yahoo.com', 'Your grade is good', '(6/11/2022)');
+INSERT INTO `reminders` (`id`, `student_email`, `from`, `message`) VALUES
+(3, 'renz@yahoo.com', 'teacher@yahoo.com', '70 grade mo'),
+(4, 'Cart@Yahoo.Com', 'teacher@yahoo.com', 'Nice student');
 
 -- --------------------------------------------------------
 
@@ -107,13 +106,7 @@ CREATE TABLE `students` (
 --
 
 INSERT INTO `students` (`id`, `f_name`, `l_name`, `email`, `password`, `grade`, `status`, `card`) VALUES
-(82, 'Idaline', 'Dossit', 'idossit0@japanpost.jp', 'JzrBeoT', 'Grade 3', 'student', 'NO RECORD'),
-(83, 'Artair', 'Applebee', 'aapplebee2@zimbio.com', 'ZA9OHJb', 'Grade 2', 'student', 'NO RECORD'),
-(84, 'Tish', 'Heustace', 'theustace3@infoseek.co.jp', 'RQRFhXA', 'Grade 6', 'student', 'NO RECORD'),
-(85, 'Rica', 'Bamfield', 'rbamfield5@xrea.com', '	Swaxo6', 'Grade 1', 'student', 'RECORDED'),
-(86, 'Laure', 'Layzell', 'llayzell7@etsy.com', 'IdkoTt7sj', 'Grade 2', 'student', 'NO RECORD'),
-(87, 'Patten', 'Ullyott	', 'pullyott8@google.co.jp', 'zp7EzNImV00', 'Grade 3', 'student', 'NO RECORD'),
-(89, 'renz', 'capena', 'renz@yahoo.com', 'renz', 'Grade 4', 'student', 'RECORDED');
+(78, 'cart', 'zy', 'cart@yahoo.com', 'cart', 'Grade 2', 'student', 'RECORDED');
 
 -- --------------------------------------------------------
 
@@ -135,11 +128,8 @@ CREATE TABLE `teachers` (
 --
 
 INSERT INTO `teachers` (`id`, `f_name`, `l_name`, `email_teacher`, `password_teacher`, `status`) VALUES
-(53, 'Hillery', 'Meaney', 'hmeaney0@n1', 'hillery1231', 'teacher'),
-(55, 'Bevan', 'Struttman', 'bstruttman2@xrea.com', '9OtWssKLEy', 'teacher'),
-(56, 'Fernande', 'Hatfield', 'fhatfield3@wired.com', 'ZZDPbQksM', 'teacher'),
-(59, 'felix', 'sam', 'teacher@yahoo.com', 'teacher', 'teacher'),
-(60, 'Jinas', 'Hayusa', 'jinhayusa@yah.giv', 'jinawd', 'teacher');
+(48, 'felix', 'delacruz', 'felix@yahoo.com', 'felix123', 'teacher'),
+(49, 'saitama', 'jun', 'teacher@yahoo.com', 'teacher', 'teacher');
 
 -- --------------------------------------------------------
 
@@ -203,25 +193,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `grades`
 --
 ALTER TABLE `grades`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `reminders`
 --
 ALTER TABLE `reminders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=79;
 
 --
 -- AUTO_INCREMENT for table `teachers`
 --
 ALTER TABLE `teachers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=61;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
 
 --
 -- AUTO_INCREMENT for table `users`
